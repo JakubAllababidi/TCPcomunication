2baf2848266f082f18db981df432a1306ab24077
JakubAllababidi <jakuballababidi@gmail.com>
2025-02-13 11:40:50 +0100

    Update MainServer.java

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -11,6 +11,9 @@ public class MainServer {
      public static void main(String[] args) {
          Server s1 = new Server(1024);
          s1.attendi();
+         s1.leggi();
+         s1.chiudi();
+         s1.termina();
    
     }
     
