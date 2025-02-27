3d510421c0f76e426d063a87bc5b57819fbb33ce
JakubAllababidi <jakuballababidi@gmail.com>
2025-02-27 11:35:19 +0100

    Update MainClient.java

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -6,8 +6,8 @@ public class MainClient {
      public static void main(String[] args) {
          Client c1 = new Client("Host");
          c1.connetti("localhost",1024);
+         c1.scrivi("messaggio prova");
          c1.leggi();
-         c1.scrivi();
          c1.chiudi();
          c1.termina();
    
